5b8a461c4eb5cf239fb8ac1d60e7121354e3b7d8
Igor Dermanovsky <igorderman@gmail.com>
2020-11-10 01:12:38 +0300

    sdelal overlay na hero section i overlay na order

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//SLIDER
+
 // получаем элементы.
 let arrowLeft = document.querySelector('#prev');
 let arrowRight = document.querySelector('#next');
@@ -41,3 +43,87 @@ currentRight = maxRight;
 }
 items.style.right = currentRight + 'px';
 })
+
+// OVERLAY ПО КНОПКЕ ЗАКАЗАТЬ
+
+let openButton = document.querySelector('#openOverlay');
+let body = document.querySelector('body')
+
+let successOverlay = createOverlay("Сообщение отправлено");
+
+openButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.appendChild(successOverlay);
+    body.style.overflow="hidden";
+})
+
+function createOverlay (content) {
+    let overlayElement = document.createElement("div");
+    overlayElement.classList.add("overlay");
+    overlayElement.addEventListener("click", function(e) {
+        if (e.target === overlayElement) {
+          closeElement.click();
+        }
+      });
+   
+    
+
+    let containerElement = document.createElement("div");
+    containerElement.classList.add("container", "container--overlay");
+
+    let contentElement = document.createElement("div");
+    contentElement.classList.add("overlay__content");
+    contentElement.innerHTML = content;
+
+    let closeElement = document.createElement("a");
+    closeElement.classList.add("overlay__close");
+    closeElement.href = "#";
+    closeElement.textContent = "Закрыть";
+    closeElement.addEventListener ('click', function(e){
+        e.preventDefault();
+        document.body.removeChild(overlayElement);
+        body.style.overflow = "initial";
+    });
+
+    overlayElement.appendChild(containerElement);
+    containerElement.appendChild(contentElement);
+    contentElement.appendChild(closeElement);
+
+    return overlayElement;
+}
+
+
+// HAMBURGER OVERLAY
+
+const hamburger = document.querySelector('#hamburger');
+const closeNavigation = document.querySelector('.hamburger__open');
+const navigation = document.querySelector('#navigation');
+
+hamburger.addEventListener('click', function(e) {
+    e.preventDefault();
+    const currentValueNavigationDisplay = window.getComputedStyle(navigation).display;
+    if (currentValueNavigationDisplay === 'none') {
+        navigation.style.display = "flex";
+        body.style.overflow = "hidden";
+        closeNavigation.classList.add("hamburger--active");
+    }
+    if (currentValueNavigationDisplay === 'flex') {
+            navigation.style.display = 'none';
+            body.style.overflow = "initial";
+            closeNavigation.classList.remove("hamburger--active");
+    }
+}
+)
+    
+    
+ 
+    
+    
+    
+    // console.log(window.getComputedStyle(navigation).display)
+// })
+// closeNavigation.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     navigation.style.display = "none";
+//     closeNavigation.classList.remove("hamburger--active");
+// })
